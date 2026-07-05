62da54cf1f2ff19fc3a5a52c098f2a7c5ec9109e
Giuseppe Corrao <josephcorr99@gmail.com>
2026-07-06 01:24:54 +0200

    feat: implemented Device Repository method's

--- src/main/java/com/Pulse/NetPulse/repository/DeviceRepository.java
+++ src/main/java/com/Pulse/NetPulse/repository/DeviceRepository.java
@@ -1,4 +1,24 @@
 package com.Pulse.NetPulse.repository;
 
-public interface DeviceRepository {
+import com.Pulse.NetPulse.model.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+
+
+    //Get a List of Device saved on DB
+    @Query(value = "Select * from Devices")
+    List<Device> findAll();
+
+    //Get a List of Device's name saved on DB
+    @Query(value = "select d.name from Devices d")
+    List<String> getAllNameDevice();
+
+    Device findById(long id);
+
 }
