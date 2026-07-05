26e7761b06f3606ffce91fe45c43dfbafc55dc88
Giuseppe Corrao <josephcorr99@gmail.com>
2026-07-05 23:16:47 +0200

    feat: implemented ip,name,username,password field on Device

--- src/main/java/com/Pulse/NetPulse/model/Device.java
+++ src/main/java/com/Pulse/NetPulse/model/Device.java
@@ -1,4 +1,17 @@
 package com.Pulse.NetPulse.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
 public class Device {
+
+    String ip;
+    String name;
+    String username;
+    String password;
+
 }
