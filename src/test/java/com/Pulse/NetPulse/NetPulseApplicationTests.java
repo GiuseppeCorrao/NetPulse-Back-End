a323b3951bfcb240e79c9094d9eaa8ed4a754c5c
Giuseppe Corrao <josephcorr99@gmail.com>
2026-07-05 22:43:34 +0200

    feat: first commit, implemented tamplate with Spring-boot (Java 21)

---
+++ src/test/java/com/Pulse/NetPulse/NetPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pulse.NetPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
