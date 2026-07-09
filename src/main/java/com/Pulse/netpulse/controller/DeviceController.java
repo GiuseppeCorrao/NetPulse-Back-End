88ae329bead1c9c029822eb77ab30b42a388429b
Giuseppe Corrao <josephcorr99@gmail.com>
2026-07-10 00:13:24 +0200

    fix: add Deprecated annotation to: DeviceController.java, MonitoringController.java,DeviceCompleteInformationDTO.java,DeviceStatusRepository.java

--- src/main/java/com/Pulse/netpulse/controller/DeviceController.java
+++ src/main/java/com/Pulse/netpulse/controller/DeviceController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@Deprecated
 @RestController
 @RequestMapping("/api/devices")
 @Tag(name = "Device API")
--- src/main/java/com/Pulse/netpulse/model/DeviceStatus.java
+++ src/main/java/com/Pulse/netpulse/model/DeviceStatus.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Deprecated
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
